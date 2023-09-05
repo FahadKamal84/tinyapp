5d4cea7e6d9bf494f1a4259697bdb00f074f958d
Fahad Kamal <fahadkamal@hotmail.com>
2023-09-04 22:01:21 -0400

    created a submission form and GET ROUTE and  POST ROUTE

--- express_server.js
+++ express_server.js
@@ -4,10 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+function generateRandomString(length = 6) {
+  const result = Math.random().toString(36).substring(2,8);
+  return result;
+
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,9 +34,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
